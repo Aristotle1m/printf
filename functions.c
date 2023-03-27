75a1cd2aba67804348aae5802b1bc345d6255119
Oki-idemudia Brenda <brenda4reala@yahoo.com>
2023-03-27 04:19:59 -0700

    updated

--- functions.c
+++ functions.c
@@ -2,7 +2,6 @@
 
 /************************* PRINT CHAR *************************/
 
-
 /**
  * print_char - Prints a char
  * @types: List a of arguments
@@ -13,17 +12,14 @@
  * @size: Size specifier
  * Return: Number of chars printed
  */
-
 int print_char(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	char c = va_arg(types, int);
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
-
 /************************* PRINT A STRING *************************/
-
 /**
  * print_string - Prints a string
  * @types: List a of arguments
@@ -35,7 +31,7 @@ int flags, int width, int precision, int size)
  * Return: Number of chars printed
  */
 int print_string(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	int length = 0, i;
 	char *str = va_arg(types, char *);
@@ -45,7 +41,6 @@ int flags, int width, int precision, int size)
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
-
 	if (str == NULL)
 	{
 		str = "(null)";
@@ -76,11 +71,10 @@ int flags, int width, int precision, int size)
 			return (width);
 		}
 	}
+
 	return (write(1, str, length));
 }
-
 /************************* PRINT PERCENT SIGN *************************/
-
 /**
  * print_percent - Prints a percent sign
  * @types: Lista of arguments
@@ -91,9 +85,8 @@ int flags, int width, int precision, int size)
  * @size: Size specifier
  * Return: Number of chars printed
  */
-
 int print_percent(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	UNUSED(types);
 	UNUSED(buffer);
@@ -102,11 +95,9 @@ int flags, int width, int precision, int size)
 	UNUSED(precision);
 	UNUSED(size);
 	return (write(1, "%%", 1));
-
 }
 
 /************************* PRINT INT *************************/
-
 /**
  * print_int - Print int
  * @types: Lista of arguments
@@ -117,9 +108,8 @@ int flags, int width, int precision, int size)
  * @size: Size specifier
  * Return: Number of chars printed
  */
-
 int print_int(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
 	int is_negative = 0;
@@ -139,17 +129,19 @@ int flags, int width, int precision, int size)
 		num = (unsigned long int)((-1) * n);
 		is_negative = 1;
 	}
+
 	while (num > 0)
 	{
 		buffer[i--] = (num % 10) + '0';
 		num /= 10;
 	}
+
 	i++;
+
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
 /************************* PRINT BINARY *************************/
-
 /**
  * print_binary - Prints an unsigned number
  * @types: Lista of arguments
@@ -160,9 +152,8 @@ int flags, int width, int precision, int size)
  * @size: Size specifier
  * Return: Numbers of char printed.
  */
-
 int print_binary(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	unsigned int n, m, i, sum;
 	unsigned int a[32];
